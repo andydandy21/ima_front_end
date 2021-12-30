ac38750478b8e42091c3926fa4c9459517d0100c
Andrew Smith <a.smithb21@gmail.com>
2021-12-30 16:51:14 -0600

    prevented login and registration pages while logged in

--- src/components/AccountLayout.js
+++ src/components/AccountLayout.js
@@ -1,6 +1,16 @@
-import { Outlet } from "react-router-dom";
+import React from "react";
+import { Outlet, useNavigate } from "react-router-dom";
 
 export default function AccountLayout() {
+    
+    const navigate = useNavigate()
+
+    React.useEffect(() => {
+        if (localStorage.getItem('access_token')) {
+            navigate('/')
+        }
+    }, [])
+
     return (
         <div className="account-layout">
             <Outlet />
